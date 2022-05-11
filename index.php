375bcf4c44b565462a3e07ad79730aaf0b9cc9fa
sachin2022apr-cedcommerce <sachin2022apr@cedcoss.com>
2022-05-11 13:37:42 +0530

    Write a program to show day of the week (for example: Monday) based on numbers using switch/case statements

--- index.php
+++ index.php
@@ -1,20 +1,31 @@
 <?php
-$marks=58;
-if($marks>=60)
+$day=6;
+switch($day)
 {
-    $result='First Division';
+    
+    case 1:
+        echo "Monday";
+        break;
+    case 2:
+        echo "Tuesday";
+        break;
+    case 3:
+        echo "Wednesday";
+        break;
+    case 4:
+        echo "Thursday";
+        break;
+    case 5:
+        echo "Friday";
+        break;
+    case 6:
+        echo "Saturday";
+        break;
+    case 7:
+        echo "Sunday";
+        break;
+    default:
+        echo "Invalid Number";
 }
-else if($marks >=45 && $marks<=59)
-{
-    $result='Second Division';
-}
-else if($marks >=33 && $marks <=44)
-{
-    $result='Third Division';
-}
-else if($marks <33)
-{
-    $result='Fail';
-}
-echo "Student Result is "."$result";
-?>
+
+?> 
