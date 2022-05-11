d97f7289597217d3249b64dfcf459510d476fa5b
sachin2022apr-cedcommerce <sachin2022apr@cedcoss.com>
2022-05-11 14:38:55 +0530

    Write a PHP program using nested for loop that creates a chess board

--- index.php
+++ index.php
@@ -1,11 +1,15 @@
 <?php
-$num=6;
-$copy=$num;
-$factorial=1;
-    while($copy!=1){
-        $factorial *= $copy;
-        $copy--;
-    }
-echo "factorial of ".$num." = ".$factorial;
-
-?> 
+    echo "<table width=400px height=400px border=2px>";
+        for($i=0; $i<8; $i++){
+            echo "<tr>";
+            for($j=0;$j<8;$j++){
+                $total=$i+$j;
+                if($total%2 == 0)
+                    echo "<td height=30px width=30px bgcolor=black></td>";
+                else if($total%2 != 0)
+                    echo "<td height=30px width=30px bgcolor=white></td>";
+             }
+             echo "</tr>";
+         }
+    echo "</table>";
+?>
