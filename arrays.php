2175e2cc5236cf38cb2f1e4d6474bfc302301a2f
sachin2022apr-cedcommerce <sachin2022apr@cedcoss.com>
2022-05-12 11:01:29 +0530

    Write a PHP script to calculate and display average temperature, five lowest and highest temperatures.

--- arrays.php
+++ arrays.php
@@ -1,15 +1,39 @@
 <?php
-    echo "Original Array:<br>";
-    $array = array('1','2','3','4','5');
-    foreach($array as $i)
+    echo "Recorded Temperatures:<br>";
+    $temprature_array = array( 78,60,62,68,71,68,73,85,66,64,76,63,75,76,73,68,62,73,72,65,74,62,62,65,64,68,73,75,79,73);
+    
+    foreach($temprature_array as $x)
     {
-        echo $i."  ";
+        echo $x."  ";
+    }
+   
+    $array_length=count($temprature_array);
+    
+    $avg = 0;
+    foreach($temprature_array as $x)
+    {
+        $avg=$avg+$x;
+    }
+
+    $avg=$avg/$array_length;
+
+    echo "<br><br>Average Temperature: ";
+    echo $avg."<br>";
+
+    sort($temprature_array);
+    echo "<br> Sorted array : <br>";
+    foreach($temprature_array as $x)
+    {
+        echo $x."  ";
     }
     
-    array_splice($array,3,0,"$");
-    echo "<br>After inserting '$' the array is :<br>";
-    foreach($array as $i)
-    {
-        echo $i."  ";
+    echo "<br><br>List of five lowest temperatures : <br>";
+    for($i=0;$i<5;$i++){
+        echo $temprature_array[$i]."  ";
+    }
+
+    echo "<br><br>List of five highest temperatures :  <br>";
+    for($i=count($temprature_array)-5;$i<=count($temprature_array);$i++){
+        echo $temprature_array[$i]."  ";
     }
 ?>
